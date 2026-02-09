87b081276ba55587a4f4e2830b5424684fb2896b
Ary Fenitra <aryfenitraraf@gmail.com>
2026-02-09 11:38:47 +0100

    Initialisation back + front

---
+++ authback/src/main/java/com/authbackend/authback/AuthbackApplication.java
@@ -0,0 +1,13 @@
+package com.authbackend.authback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthbackApplication.class, args);
+	}
+
+}
