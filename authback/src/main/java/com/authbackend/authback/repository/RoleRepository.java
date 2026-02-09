52a6a03f5be8a733871c003a204a6146fac0c137
Ary Fenitra <aryfenitraraf@gmail.com>
2026-02-09 14:18:21 +0100

    creation de UserRepository et RoleRepository

---
+++ authback/src/main/java/com/authbackend/authback/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.authbackend.authback.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.authbackend.authback.entity.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    Optional<Role> findByName(String name);
+}
