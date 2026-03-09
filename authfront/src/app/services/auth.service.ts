7af6f6ea41aa1235a959ff8403119887d81f1497
Ary Fenitra <aryfenitraraf@gmail.com>
2026-03-09 23:06:13 +0100

    changement de la declaration de variable dans auth.service.ts

--- authfront/src/app/services/auth.service.ts
+++ authfront/src/app/services/auth.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { finalize, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = `${environment.apiUrl}/auth`;
+
+  private apiUrl = environment.apiUrl + '/auth';
+  //private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient){
   }
